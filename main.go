096c4462a2b885af6b577eef274421422885c978
Mahir Labib Chowdhury <optical.mahir@gmail.com>
2021-01-19 04:45:47 +0600

    changed version number to RC status!!!

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ import (
 )
 
 // Version is a constant that stores the Disgord version information.
-const Version = "v0.0.0-alpha"
+const Version = "v0.9.0-rc"
 
 // Session is declared in the global space so it can be easily used
 // throughout this program.
